99b5a9967444b08c9b1729001b1b4b0b21493239
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2024-04-26 21:28:10 +0300

    Add Interface for scormApi

--- cryptifyhub-app/src/shared/libs/scorm.ts
+++ cryptifyhub-app/src/shared/libs/scorm.ts
@@ -1,17 +1,24 @@
 import { SCORM } from 'pipwerks-scorm-api-wrapper';
 
-export const Scorm = {
+interface IScorm {
+	/** Initialization SCORM */
+	init(): void;
+	/** Method to calculate score. Score is value from results  */
+	calculateScore(score: number): void;
+	/** Finish SCORM */
+	finish(): void;
+}
+
+export const Scorm: IScorm = {
 	init() {
 		SCORM.init();
 	},
-
-	calculateScore(score: number) {
+	calculateScore(score) {
 		SCORM.set("cmi.core.score.raw", `${score}`);
 		SCORM.set("cmi.core.score.max", "100");
 		SCORM.set("cmi.core.score.min", "0");
 		SCORM.save();
 	},
-
 	finish() {
 		SCORM.set("cmi.core.lesson_status", "completed");
 		SCORM.save();
